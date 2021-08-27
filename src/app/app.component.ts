48f9e05b0b8a55faddc3fbfeac15ed8a6747e470
unknown <Brunogoffredo22@gmail.com>
2021-08-27 00:04:07 -0300

    Primer commit. Modificaciones para deploy heroku

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Resultados } from './clases/resultados';
+import { Usuario } from './clases/usuario';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  numero1:number = 0;
+  numero2:number = 0;
+  resultado:Resultados;
+  
+  constructor(){
+    this.resultado = new Resultados(0,0)
+  }
+  Calcular(){
+    this.resultado.suma = this.numero1 + this.numero2;
+    this.resultado.promedio = (this.numero1 + this.numero2)/2;
+  };
+  Limpiar(){
+    this.numero1 = 0;
+    this.numero2 = 0;
+    this.Calcular();
+
+  };  
+}
---
+++ src/app/clases/resultados.ts
@@ -0,0 +1,9 @@
+export class Resultados {
+    suma:number;
+    promedio:number;
+
+    constructor(suma:number,promedio:number){
+        this.suma = suma;
+        this.promedio = promedio;
+    }
+}
